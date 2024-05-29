228873b3999eea27ba9387a64a27b5c5a539c3e1
Irving LLerena Bejarano <irvingllerena@gmail.com>
2024-05-29 11:19:13 -0500

    se quito el simbolo del costado

--- src/components/switcher.js
+++ src/components/switcher.js
@@ -58,12 +58,12 @@ export default function Switcher(){
             </span>
         </div>
 
-        <div className="fixed top-[40%] -right-3 z-50">
+        {/* <div className="fixed top-[40%] -right-3 z-50">
             <Link to="" id="switchRtl">
                 <span className="py-1 px-3 relative inline-block rounded-t-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-800 font-medium rtl:block ltr:hidden" onClick={(e)=>modeChange(e)}>LTR</span>
                 <span className="py-1 px-3 relative inline-block rounded-t-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-800 font-medium ltr:block rtl:hidden" onClick={(e)=>modeChange(e)}>RTL</span>
             </Link>
-        </div>
+        </div> */}
         </>
     )
 }
