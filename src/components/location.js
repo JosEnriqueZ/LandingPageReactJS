dbeb3b997c63158c0dfae474f97d35243b0f8589
Irving LLerena Bejarano <irvingllerena@gmail.com>
2024-06-06 17:09:24 -0500

    location grilla mapa y direccion

--- src/components/location.js
+++ src/components/location.js
@@ -1,77 +1,35 @@
-import React, { useState } from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
 
-// import ctaImage from "../assets/images/home.png"
-
-// import ModalVideo from 'react-modal-video';
-// import "../../node_modules/react-modal-video/scss/modal-video.scss";
-
-export default function Cta() {
+export default function Location() {
   return (
-    <section className="relative md:py-24 py-16 md:pt-0 pt-0 bg-gradient-to-b from-teal-400 to-teal-500">
+    <section className="relative md:py-24 py-16 bg-gradient-to-b from-teal-400 to-teal-500">
       <div className="container relative">
-        <div className="grid grid-cols-1 justify-center">
-          <div className="relative z-1">
-          <div className="content md:mt-8">
-              <div className="grid lg:grid-cols-12 grid-cols-1 md:text-start text-center justify-center">
-                <div className="lg:col-start-2 lg:col-span-10">
-                  <div className="items-center">
-                    <div className="mt-8 mb-16">
-                      <div className="section-title text-md-start">
-                        {/* <h6 className="text-white/70 text-sm font-semibold uppercase">Get Free Trial</h6> */}
-                        <h3 className="font-semibold text-2xl leading-normal text-white mt-2 text-center">
-                          Nuestra Localizacion
-                        </h3>
-                      </div>
-                    </div>
-
-                    {/* <div className="mt-8">
-                                            <div className="section-title text-md-start">
-                                                <p className="text-white/70 max-w-xl mx-auto mb-2">Mapa</p>
-                                                <Link to="" className="text-white">Read More <i className="mdi mdi-chevron-right align-middle"></i></Link>
-                                            </div>
-                                        </div> */}
-                  </div>
-                </div>
-              </div>
+        <h3 className="font-semibold text-2xl leading-normal text-white mt-2 text-center">
+          Nuestra Localizacion
+        </h3>
+        <div className="grid lg:grid-cols-2 md:grid-cols-1 mt-8">
+          <div className="flex justify-center w-full h-48 md:h-[25vh] md:w-full lg:h-[30vh] lg:w-[50%] relative">
+            <div className="w-full h-full border-0">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491307.3440214395!2d-72.23031947708112!3d-15.83363773922691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9141f6ecdc90dbcd%3A0x35fe3e38d4c7862e!2sDistrito%20de%20Lluta%2004115!5e0!3m2!1ses-419!2spe!4v1717711588917!5m2!1ses-419!2spe"
+                className="w-full h-full rounded-xl"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Google Map"
+              ></iframe>
             </div>
-            <div className="grid grid-cols-1 md:text-start text-center md:text-start">
-              <div className="flex justify-center items-center">
-                <div className="flex justify-center items-center w-full h-96 md:h-[50vh] lg:h-[60vh] relative">
-                  <div className="w-full h-full border-0 max-w-screen-lg">
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51579.98135852402!2d-72.15919858776121!3d-16.08078830006455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9141e94b170ae7ed%3A0x27ea7b798e0af463!2sCampamento%20Zafranal!5e0!3m2!1ses!2spe!4v1717302865596!5m2!1ses!2spe"
-                      className="w-full h-full rounded-xl"
-                      style={{ border: 0 }}
-                      allowFullScreen=""
-                      loading="lazy"
-                      referrerPolicy="no-referrer-when-downgrade"
-                      title="Google Map"
-                    ></iframe>
-                  </div>
-                </div>
-                {/* <img src={ctaImage} alt=""/> */}
-                {/* <div className="absolute bottom-2/4 translate-y-2/4 start-0 end-0 text-center">
-                                <Link to="#!" onClick={() => setOpen(true)}
-                                    className="lightbox size-20 rounded-full shadow-lg dark:shadow-gray-700 inline-flex items-center justify-center bg-white dark:bg-slate-900 text-teal-500 dark:text-white">
-                                    <i className="mdi mdi-play inline-flex items-center justify-center text-2xl"></i>
-                                </Link>
-                            </div> */}
-                {/* <ModalVideo
-                                channel="youtube"
-                                youtube={{ mute: 0, autoplay: 0 }}
-                                isOpen={isOpen}
-                                videoId="S_CGed6E610"
-                                onClose={() => setOpen(false)} 
-                            /> */}
-              </div>
+          </div>
+          <div className="flex justify-center w-full">
+            <div className="lg:mt-4 lg:mb-16 lg:ml-16">
+              <p className="text-white text-lg  mt-8">Nos ubicamos en el distrito de Lluta, provincia de Caylloma</p>
+              <p className="text-white text-xl"><strong>Direccion:</strong> C. P. La colina E 13, Carretera a Lluta, Distrito de Majes Provincia: Caylloma</p>
             </div>
-
           </div>
         </div>
-      </div>
 
-      {/* <div className="absolute bottom-0 start-0 end-0 sm:h-2/3 h-4/5 bg-gradient-to-b from-teal-400 to-teal-500"></div> */}
+      </div>
     </section>
   );
 }
