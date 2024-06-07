dc6115ea6a7697aa14ca94324fd5c1d29df01b03
Irving LLerena Bejarano <irvingllerena@gmail.com>
2024-06-06 22:06:29 -0500

    formulario front contactenos

--- src/components/getInTuoch.js
+++ src/components/getInTuoch.js
@@ -1,8 +1,39 @@
 import React from "react";
 // import contact from '../assets/images/contact.svg'
+import { useState } from 'react';
 import contact from '../assets/images/contact.webp'
 
 export default function GetInTouch(){
+    const fromEmail ="notificacion@silingcorp.com.pe"
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        phone: '',
+        email: '',
+        from: fromEmail,
+        subject: '',
+        comments: ''
+    });
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('https://email.silingcorp.com.pe/send-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const result = await response.json();
+            alert(result.message);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Error al enviar el mensaje');
+        }
+    };
     return(
         <section className="relative lg:py-24 py-16 bg-slate-50 dark:bg-slate-800" id="contact">
             <div className="container relative">
@@ -26,26 +57,34 @@ export default function GetInTouch(){
                     <div className="lg:col-span-5 md:col-span-6">
                         <div className="lg:ms-5">
                             <div className="bg-white dark:bg-slate-900 rounded-md shadow dark:shadow-gray-700 p-6">
-                                <form method="post" >
+                                <form onSubmit={handleSubmit}>
                                     <div className="grid lg:grid-cols-12 grid-cols-1 gap-3">
                                         <div className="lg:col-span-6">
-                                            <label htmlFor="name" className="font-semibold">Nombre:</label>
-                                            <input name="name" id="name" type="text" className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Nombre :"/>
+                                            <label htmlFor="firstname" className="font-semibold">Nombre:</label>
+                                            <input name="firstname" id="firstname" type="text" value={formData.firstname} onChange={handleChange}  className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Nombre :"/>
+                                        </div>
+                                        <div className="lg:col-span-6">
+                                            <label htmlFor="lastname" className="font-semibold">Apellido:</label>
+                                            <input name="lastname" id="lastname" type="text" value={formData.lastname} onChange={handleChange} className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Apellido :"/>
+                                        </div>
+                                        <div className="lg:col-span-6">
+                                            <label htmlFor="phone" className="font-semibold">Celular:</label>
+                                            <input name="phone" id="phone" type="text"  value={formData.phone} onChange={handleChange} className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Celular :"/>
                                         </div>
         
                                         <div className="lg:col-span-6">
                                             <label htmlFor="email" className="font-semibold">Correo Electronico:</label>
-                                            <input name="email" id="email" type="email" className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Correo Electronico :"/>
+                                            <input name="email" id="email" type="email"  value={formData.email} onChange={handleChange} className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Correo Electronico :"/>
                                         </div>
 
                                         <div className="lg:col-span-12">
                                             <label htmlFor="subject" className="font-semibold">Pregunta:</label>
-                                            <input name="subject" id="subject" className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Asunto :"/>
+                                            <input name="subject" id="subject" value={formData.subject} onChange={handleChange} className="mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Asunto :"/>
                                         </div>
     
                                         <div className="lg:col-span-12">
                                             <label htmlFor="comments" className="font-semibold">Comentario:</label>
-                                            <textarea name="comments" id="comments" className="mt-2 w-full py-2 px-3 h-28 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Mensaje :"></textarea>
+                                            <textarea name="comments" id="comments" value={formData.comments} onChange={handleChange} className="mt-2 w-full py-2 px-3 h-28 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-100 dark:border-gray-800 focus:ring-0" placeholder="Mensaje :"></textarea>
                                         </div>
                                     </div>
                                     <button type="submit" id="submit" name="send" className="h-10 px-6 tracking-wide inline-flex items-center justify-center font-medium rounded-md bg-teal-500 text-white mt-2">Enviar Mensaje</button>
